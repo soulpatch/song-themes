8e7be85a1dc4ee728c2ffa0e39ec3384b52b41d2
Mike Rizzi <mikerizzi@gmail.com>
2023-12-12 10:06:55 -0800

    Initial commit

---
+++ src/main/java/com/songthematic/songthemes/SongThemesApplication.java
@@ -0,0 +1,13 @@
+package com.songthematic.songthemes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongThemesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SongThemesApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/songthematic/songthemes/TestSongThemesApplication.java
@@ -0,0 +1,23 @@
+package com.songthematic.songthemes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestSongThemesApplication {
+
+  @Bean
+  @ServiceConnection
+  PostgreSQLContainer<?> postgresContainer() {
+    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.from(SongThemesApplication::main).with(TestSongThemesApplication.class).run(args);
+  }
+
+}
