68a840d9bae7155b8b4d0d9918ce4b88dbc03f0d
Mike Rizzi <mikerizzi@gmail.com>
2023-12-19 15:52:50 -0800

    Failing test for theme-search endpoint

---
+++ src/test/java/com/songthematic/songthemes/SongThemesMvcTest.java
@@ -0,0 +1,22 @@
+package com.songthematic.songthemes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class SongThemesMvcTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void postToSearchEndpointRedirects() throws Exception {
+        mockMvc.perform(post("/theme-search"))
+                .andExpect(status().is3xxRedirection());
+    }
+}
