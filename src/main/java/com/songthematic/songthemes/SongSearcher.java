86fc229d6295312b1e4e69e3adce7b3b24658188
Mike Rizzi <mikerizzi@gmail.com>
2023-12-19 15:43:11 -0800

    Added first test for searching songs by theme

---
+++ src/main/java/com/songthematic/songthemes/SongSearcher.java
@@ -0,0 +1,10 @@
+package com.songthematic.songthemes;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SongSearcher {
+    public List<String> byTheme(String theme) {
+        return Collections.emptyList();
+    }
+}
---
+++ src/test/java/com/songthematic/songthemes/SongSearchByThemeTest.java
@@ -0,0 +1,19 @@
+package com.songthematic.songthemes;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SongSearchByThemeTest {
+    @Test
+    public void searchForThemeThatDoesNotExistReturnsNoResults() throws Exception {
+        SongSearcher songSearcher = new SongSearcher();
+
+        List<String> foundSongs = songSearcher.byTheme("Applesauce");
+
+        assertThat(foundSongs)
+                .isEmpty();
+    }
+}
