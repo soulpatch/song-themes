162150b916585077a7d89516d46c838eaff087df
Mike Rizzi <mikerizzi@gmail.com>
2023-12-23 15:49:46 -0800

    Properly handle case-insensitive theme search

--- src/main/java/com/songthematic/songthemes/SongSearcher.java
+++ src/main/java/com/songthematic/songthemes/SongSearcher.java
@@ -1,23 +1,27 @@
 package com.songthematic.songthemes;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SongSearcher {
 
     private final String theme;
     private final String song;
+    private final Map<String, Song> songs = new HashMap<>();
 
     private SongSearcher(Song... song) {
         this.theme = song[0].theme();
         this.song = song[0].title();
+        songs.put(song[0].theme().toLowerCase(), song[0]);
     }
 
     public static SongSearcher createSongSearcher(Song... songs) {
-        return new SongSearcher(songs[0]);
+        return new SongSearcher(songs);
     }
 
-    public static SongSearcher withSongsForTheme(String theme) {
+    public static SongSearcher withOneSongForTheme(String theme) {
         return new SongSearcher(new Song(theme, "Song with theme "+ theme));
     }
 
@@ -26,8 +30,10 @@ public class SongSearcher {
     }
 
     public List<String> byTheme(String requestedTheme) {
-        if (requestedTheme.equalsIgnoreCase(theme)) {
-            return List.of(song);
+        Song song = songs.get(requestedTheme.toLowerCase());
+
+        if (song != null) {
+            return List.of(song.title());
         }
         return Collections.emptyList();
     }
--- src/test/java/com/songthematic/songthemes/SongSearchByThemeTest.java
+++ src/test/java/com/songthematic/songthemes/SongSearchByThemeTest.java
@@ -1,6 +1,9 @@
 package com.songthematic.songthemes;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -10,7 +13,7 @@ public class SongSearchByThemeTest {
 
     @Test
     public void searchForThemeThatDoesNotExistReturnsNoResults() throws Exception {
-        SongSearcher songSearcher = SongSearcher.withSongsForTheme("new years");
+        SongSearcher songSearcher = SongSearcher.withOneSongForTheme("new years");
 
         List<String> foundSongs = songSearcher.byTheme("Applesauce");
 
@@ -18,13 +21,31 @@ public class SongSearchByThemeTest {
                 .isEmpty();
     }
 
-    @Test
-    void searchForThemeFindsOneMatchingSong() throws Exception {
-        SongSearcher songSearcher = SongSearcher.createSongSearcher(new Song("new years", "auld lang syne"));
+    @ParameterizedTest()
+    @CsvSource({
+            "new years,New Years",
+            "new years,new years",
+            "New Years,New Years",
+            "New Years,new years"
+    })
+    void searchForThemeFindsOneMatchingSong(String songTheme, String requestedTheme) throws Exception {
+        SongSearcher songSearcher = SongSearcher.createSongSearcher(new Song(songTheme, "auld lang syne"));
 
-        List<String> foundSong = songSearcher.byTheme("New Years");
+        List<String> foundSong = songSearcher.byTheme(requestedTheme);
 
         assertThat(foundSong)
                 .containsExactly("auld lang syne");
+    }
+
+    @Test
+    @Disabled
+    void searchForThemeFindsMultipleMatchingSongs() throws Exception {
+        SongSearcher songSearcher = SongSearcher.createSongSearcher(new Song("new years", "auld lang syne"),
+                                                                    new Song("new years", "New Year's Eve In A Haunted House"));
+        List<String> foundSongs = songSearcher.byTheme("New Years");
+
+        assertThat(foundSongs)
+                .containsExactly("auld lang syne",
+                                 "New Year's Eve In A Haunted House");
     }
 }
