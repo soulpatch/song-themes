762de66d9da68e14e0edf89cb7a3ad54b415fa75
Mike Rizzi <mikerizzi@gmail.com>
2023-12-30 15:18:41 -0800

    First cut at searching by theme at the service level

---
+++ src/main/java/com/songthematic/songthemes/application/SongService.java
@@ -0,0 +1,12 @@
+package com.songthematic.songthemes.application;
+
+import com.songthematic.songthemes.domain.Song;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SongService {
+    public List<Song> searchByTheme(String theme) {
+        return Collections.emptyList();
+    }
+}
---
+++ src/test/java/com/songthematic/songthemes/application/SongServiceTest.java
@@ -0,0 +1,21 @@
+package com.songthematic.songthemes.application;
+
+import com.songthematic.songthemes.domain.Song;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SongServiceTest {
+
+    @Test
+    void noSongsAddedThenNoSongsFound() throws Exception {
+        SongService songService = new SongService();
+
+        List<Song> songsFound = songService.searchByTheme("new years");
+
+        assertThat(songsFound)
+                .isEmpty();
+    }
+}
