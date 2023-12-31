27f28651b78f2c3da8a81a63615b10cb6c4cf3ec
Mike Rizzi <mikerizzi@gmail.com>
2023-12-31 09:19:50 -0800

    Removed unneeded service level tests

--- src/test/java/com/songthematic/songthemes/application/SongServiceTest.java
+++ src/test/java/com/songthematic/songthemes/application/SongServiceTest.java
@@ -10,27 +10,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SongServiceTest {
 
     @Test
-    void noSongsAddedThenNoSongsFound() throws Exception {
-        SongService songService = new SongService();
-
-        List<Song> songsFound = songService.searchByTheme("new years");
-
-        assertThat(songsFound)
-                .isEmpty();
-    }
-
-    @Test
-    void oneSongAddedIsFoundByItsTheme() throws Exception {
-        SongService songService = new SongService();
-
-        songService.addSong(new Song("new years", "This Will Be Our Year"));
-
-        List<Song> songsFound = songService.searchByTheme("new years");
-        assertThat(songsFound)
-                .containsExactly(new Song("new years", "This Will Be Our Year"));
-    }
-
-    @Test
     void multipleSongsAddedAreFoundByTheirTheme() throws Exception {
         SongService songService = new SongService();
 
