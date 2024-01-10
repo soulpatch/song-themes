bc616fa0c785f1bd9e2e9950277980c995fdd89d
Ted M. Young <tedyoung@gmail.com>
2024-01-10 07:31:21 -0800

    Update to Text Blocks and CSV test data

--- src/test/java/com/songthematic/songthemes/application/CsvSongParserTest.java
+++ src/test/java/com/songthematic/songthemes/application/CsvSongParserTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class CsvSongParserTest {
     @Test
     void parseSongFromCsv() throws Exception {
         String csvSongs = "\"Artist\",\"SongTitle\",\"ReleaseTitle\",\"ReleaseType\",\"Theme1\"";
 
-        /* can use text blocks, but the last double-quote has to be escaped
-        String csvSongs = """
-                          "Artist","SongTitle","ReleaseTitle","ReleaseType","Theme1\"""";
-        */
+        // can use text blocks, but the last line has to be escaped to avoid a new line at the end
+//        String csvSongs = """
+//                          "Artist","SongTitle","ReleaseTitle","ReleaseType","Theme1"\
+//                          """;
 
         CsvSongParser csvSongParser = new CsvSongParser();
         List<Song> songs = csvSongParser.parse(csvSongs);
