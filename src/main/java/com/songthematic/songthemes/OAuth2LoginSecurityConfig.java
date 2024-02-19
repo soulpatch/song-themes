e5acac23501b8e6532497d5e854f74a236a87bb5
Mike Rizzi <mikerizzi@gmail.com>
2024-02-19 12:13:08 -0800

    Added forwards and errors to permit all

--- src/main/java/com/songthematic/songthemes/OAuth2LoginSecurityConfig.java
+++ src/main/java/com/songthematic/songthemes/OAuth2LoginSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.songthematic.songthemes;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,7 +13,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class OAuth2LoginSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable());
         http.authorizeHttpRequests(authorize -> authorize
+                    .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
                     .requestMatchers("/", "/theme-search").permitAll()
                     .requestMatchers("/song-import").authenticated()
             )
