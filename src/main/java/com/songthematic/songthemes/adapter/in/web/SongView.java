05a14319b2edebab7ead7f10e380895441eae36c
Mike Rizzi <mikerizzi@gmail.com>
2024-05-22 14:49:21 -0700

    Added ReleaseType to SongView so it matches everything contained in Song

--- src/main/java/com/songthematic/songthemes/adapter/in/web/SongView.java
+++ src/main/java/com/songthematic/songthemes/adapter/in/web/SongView.java
@@ -5,12 +5,13 @@ import com.songthematic.songthemes.domain.Song;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record SongView(String artist, String title, String releaseTitle, String themes) {
+public record SongView(String artist, String title, String releaseTitle, String releaseType, String themes) {
     static List<SongView> from(List<Song> foundSongs) {
         return foundSongs.stream()
                          .map(song -> new SongView(song.artist(),
                                                    song.songTitle(),
                                                    song.releaseTitle(),
+                                                   song.releaseType(),
                                                    song.themes().stream().collect(Collectors.joining(", ", "[", "]"))))
                          .toList();
     }
--- src/test/java/com/songthematic/songthemes/adapter/in/web/SongViewTest.java
+++ src/test/java/com/songthematic/songthemes/adapter/in/web/SongViewTest.java
@@ -12,12 +12,12 @@ class SongViewTest {
     @Test
     void convertsFoundSongsToSongViews() throws Exception {
 
-        List<Song> songs = List.of(new Song("Flesh Eaters", "Digging My Grave", "A Minute To Pray A Second To Die", "IrrelevantReleaseType", List.of("Halloween", "Death", "Graveyards")),
-                                   new Song("Revillos", "Jack The Ripper", "From The Freezer", "IrrelevantReleaseType", List.of("Halloween")));
+        List<Song> songs = List.of(new Song("Flesh Eaters", "Digging My Grave", "A Minute To Pray A Second To Die", "", List.of("Halloween", "Death", "Graveyards")),
+                                   new Song("Revillos", "Jack The Ripper", "From The Freezer", "", List.of("Halloween")));
         List<SongView> songViews = SongView.from(songs);
 
         assertThat(songViews)
-                .containsExactly(new SongView("Flesh Eaters", "Digging My Grave", "A Minute To Pray A Second To Die", "[Halloween, Death, Graveyards]"),
-                                 new SongView("Revillos", "Jack The Ripper", "From The Freezer", "[Halloween]"));
+                .containsExactly(new SongView("Flesh Eaters", "Digging My Grave", "A Minute To Pray A Second To Die", "", "[Halloween, Death, Graveyards]"),
+                                 new SongView("Revillos", "Jack The Ripper", "From The Freezer", "", "[Halloween]"));
     }
 }
