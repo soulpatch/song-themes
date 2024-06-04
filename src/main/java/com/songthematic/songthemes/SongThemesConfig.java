edc11b5385454607865ed2f50d4ad9ebfd18eca6
Mike Rizzi <mikerizzi@gmail.com>
2024-06-04 10:29:41 -0700

    Song theme dropdown is populated and Search returns songs for the selected theme

--- src/main/java/com/songthematic/songthemes/SongThemesConfig.java
+++ src/main/java/com/songthematic/songthemes/SongThemesConfig.java
@@ -4,11 +4,8 @@ import com.songthematic.songthemes.adapter.out.jdbc.JdbcSongRepository;
 import com.songthematic.songthemes.adapter.out.jdbc.SongJdbcAdapter;
 import com.songthematic.songthemes.application.SongService;
 import com.songthematic.songthemes.application.port.SongRepository;
-import com.songthematic.songthemes.application.port.ThemeFinder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SongThemesConfig {
@@ -17,15 +14,5 @@ public class SongThemesConfig {
     public SongService songService(JdbcSongRepository jdbcSongRepository) {
         SongRepository songJdbcAdapter = new SongJdbcAdapter(jdbcSongRepository);
         return new SongService(songJdbcAdapter);
-    }
-
-    @Bean
-    public ThemeFinder themeFinder() {
-        return new ThemeFinder() {
-            @Override
-            public List<String> allUniqueThemesAlphabetically() {
-                return List.of();
-            }
-        };
     }
 }
